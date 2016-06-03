7a9ac0980aa3a76c37be6abb68e5cd65bcbe7aa0
eschiebel <eschiebe@us.ibm.com>
2016-06-03 13:38:52 -0400

    handle <= 5

--- src/romanizer.js
+++ src/romanizer.js
@@ -5,38 +5,36 @@ var romanizer = {
     // arabic: number
     // returns: string: roman numeral
     ator: function(arabic) {
+        var d;
         var roman = '';
-        if(arabic < 5) {
-            if(4 === arabic) {
-                roman += 'IV';
+
+        if(arabic === 1000) {
+            roman += 'M'
+        }
+        else if(arabic === 500) {
+            roman += 'D';
+        }
+        else if(arabic === 100) {
+            roman += 'C';
+        }
+        else if(arabic === 50) {
+            roman += 'L';
+        }
+        else if(arabic === 10) {
+            roman += 'X';
+        }
+        else if(arabic === 5) {
+            roman += 'V';
+        }
+        else if(arabic === 4) {
+            roman += 'IV';
+        }
+        else {
+            for(d = 0; d < arabic; ++d) { // one I for each of the rest
+                roman += 'I';
             }
-            else {
-                for(var d = 0; d < arabic; ++d) {
-                    roman += 'I';
-                }
-            }
         }
-        else if(1 === arabic) {
-            roman = 'I';
-        }
-        else if(5 === arabic) {
-            roman = 'V';
-        }
-        else if(10 === arabic) {
-            roman = 'X';
-        }
-        else if(50 === arabic) {
-            roman = 'L';
-        }
-        else if(100 === arabic) {
-            roman = 'C';
-        }
-        else if(500 === arabic) {
-            roman = 'D';
-        }
-        else if(1000 === arabic) {
-            return 'M';
-        }
+
         return roman;
     }
 };
