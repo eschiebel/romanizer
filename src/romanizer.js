4aec25c1c6d579381bc6f1327e9fc97f6c19f463
eschiebel <eschiebe@us.ibm.com>
2016-06-03 13:58:28 -0400

    backing up. this will be easier to to top down rather than bottom up. handling 1000s

--- src/romanizer.js
+++ src/romanizer.js
@@ -5,37 +5,55 @@ var romanizer = {
     // arabic: number
     // returns: string: roman numeral
     ator: function(arabic) {
-        var d;
+        var d;      // digit for looping
+        var r;      // remainders
+        var m, c, x, i;   // place values
         var roman = '';
+        //console.log("arabic:", arabic);
 
-        if(arabic === 1000) {
-            roman += 'M'
+        m = Math.floor(arabic / 1000);
+        if(m) {
+            console.log("m", m);
+            r = arabic % 1000;
+            c = r / 100;
+            roman += genDigits(m, 'M');
         }
-        else if(arabic === 500) {
-            roman += 'D';
-        }
-        else if(arabic === 100) {
-            roman += 'C';
-        }
-        else if(arabic === 50) {
-            roman += 'L';
-        }
-        else if(arabic === 10) {
-            roman += 'X';
-        }
-        else if(arabic === 5) {
-            roman += 'V';
-        }
-        else if(arabic === 4) {
-            roman += 'IV';
-        }
-        else {
-            for(d = 0; d < arabic; ++d) { // one I for each of the rest
-                roman += 'I';
-            }
-        }
-
+        // else if(arabic === 500) {
+        //     roman += 'D';
+        // }
+        // else if(arabic === 100) {
+        //     roman += 'C';
+        // }
+        // else if(arabic === 50) {
+        //     roman += 'L';
+        // }
+        // else if(arabic === 10) {
+        //     roman += 'X';
+        // }
+        // x = Math.floor(arabic / 10);
+        // if(x) {
+        //     i = x % 10;
+        // }
+        // else if(i === 5) {
+        //     roman += 'V';
+        // }
+        // else if(i === 4) {
+        //     roman += 'IV';
+        // }
+        // else {
+        //     for(d = 0; d < i; ++d) { // one I for each of the rest
+        //         roman += 'I';
+        //     }
+        // }
+        console.log('roman:', roman);
         return roman;
     }
 };
+
+function genDigits(n, c) {
+    var str = '';
+    for(var i = 0; i < n; ++i)
+        str += c;
+    return str;
+}
 module.exports = romanizer;
--- test/romanizer-test.js
+++ test/romanizer-test.js
@@ -8,40 +8,49 @@ var should = chai.should();
 
 describe('romanizer', () => {
 
-    it('should return I given 1', function () {
-        var r = romanizer.ator(1);
-        expect(r).to.be.equal('I');
-    });
-    it('should return V given 5', function () {
-        var r = romanizer.ator(5);
-        expect(r).to.be.equal('V');
-    });
-    it('should return X given 10', function () {
-        var r = romanizer.ator(10);
-        expect(r).to.be.equal('X');
-    });
-    it('should return L given 50', function () {
-        var r = romanizer.ator(50);
-        expect(r).to.be.equal('L');
-    });
-    it('should return C given 100', function () {
-        var r = romanizer.ator(100);
-        expect(r).to.be.equal('C');
-    });
-    it('should return D given 500', function () {
-        var r = romanizer.ator(500);
-        expect(r).to.be.equal('D');
-    });
+    // it('should return I given 1', function () {
+    //     var r = romanizer.ator(1);
+    //     expect(r).to.be.equal('I');
+    // });
+    // it('should return V given 5', function () {
+    //     var r = romanizer.ator(5);
+    //     expect(r).to.be.equal('V');
+    // });
+    // it('should return X given 10', function () {
+    //     var r = romanizer.ator(10);
+    //     expect(r).to.be.equal('X');
+    // });
+    // it('should return L given 50', function () {
+    //     var r = romanizer.ator(50);
+    //     expect(r).to.be.equal('L');
+    // });
+    // it('should return C given 100', function () {
+    //     var r = romanizer.ator(100);
+    //     expect(r).to.be.equal('C');
+    // });
+    // it('should return D given 500', function () {
+    //     var r = romanizer.ator(500);
+    //     expect(r).to.be.equal('D');
+    // });
     it('should return M given 1000', function () {
         var r = romanizer.ator(1000);
         expect(r).to.be.equal('M');
     });
-    it('should return III given 3', function() {
-        var r = romanizer.ator(3);
-        expect(r).to.be.equal('III');
+    it('should return MM given 2000', function () {
+        var r = romanizer.ator(2000);
+        expect(r).to.be.equal('MM');
     });
-    it('should return IV given 4', function() {
-        var r = romanizer.ator(4);
-        expect(r).to.be.equal('IV');
-    });
+
+    // it('should return III given 3', function() {
+    //     var r = romanizer.ator(3);
+    //     expect(r).to.be.equal('III');
+    // });
+    // it('should return IV given 4', function() {
+    //     var r = romanizer.ator(4);
+    //     expect(r).to.be.equal('IV');
+    // });
+    // if('should return VI given 6', function() {
+    //     var r = romanizer.ator(6);
+    //     expect(r).to.be.equal('VI');
+    // });
 })
