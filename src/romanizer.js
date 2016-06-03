55afcc3104d448b41cc68ff25ef61332263fe417
eschiebel <eschiebe@us.ibm.com>
2016-06-03 15:17:18 -0400

    handles everything but substraction situations

--- src/romanizer.js
+++ src/romanizer.js
@@ -1,32 +1,55 @@
 
 var romanizer = {
     // convert arabic in to roman out
+    // good for 0 - 4999
     // arabic: number
     // returns: string: roman numeral
+    // throws:
+    //
     ator: function(arabic) {
         var r;      // remainders
         var m, c, x, i;   // place values
         var roman = '';
         //console.log("arabic:", arabic);
 
+        if(arabic > 4999) {
+            throw new RangeError("Value must be < 5000");
+        }
+        if(arabic < 0) {
+            throw new RangeError("Value must be > 0");
+        }
+
+        // work out the digits
         r = arabic;
         m = Math.floor(arabic / 1000);
         if(m) {
             r = r % 1000;
         }
         c = Math.floor(r / 100);
-
         if(c) {
             r = r % 100;
         }
         x = Math.floor(r / 10);
+        i = r % 10;
+        //console.log(m, c, x, i);
 
+        //convert digits to roman chars
         roman += genDigits(m, 'M');
         if(c >= 5) {
             roman += 'D';
             c -= 5;
         }
         roman += genDigits(c, 'C');
+        if(x >= 5) {
+            roman += 'L';
+            x -= 5;
+        }
+        roman += genDigits(x, 'X');
+        if(i >= 5) {
+            roman += 'V'
+            i -= 5;
+        }
+        roman += genDigits(i, 'I');
 
         //console.log('roman:', roman);
         return roman;
--- test/romanizer-test.js
+++ test/romanizer-test.js
@@ -8,23 +8,7 @@ var should = chai.should();
 
 describe('romanizer', () => {
 
-    // it('should return I given 1', function () {
-    //     var r = romanizer.ator(1);
-    //     expect(r).to.be.equal('I');
-    // });
-    // it('should return V given 5', function () {
-    //     var r = romanizer.ator(5);
-    //     expect(r).to.be.equal('V');
-    // });
-    // it('should return X given 10', function () {
-    //     var r = romanizer.ator(10);
-    //     expect(r).to.be.equal('X');
-    // });
-    // it('should return L given 50', function () {
-    //     var r = romanizer.ator(50);
-    //     expect(r).to.be.equal('L');
-    // });
-
+    // test thousands
     it('should return M given 1000', function () {
         var r = romanizer.ator(1000);
         expect(r).to.be.equal('M');
@@ -33,11 +17,11 @@ describe('romanizer', () => {
         var r = romanizer.ator(2000);
         expect(r).to.be.equal('MM');
     });
+    // throw in some hundreds
     it('should return MD given ', function () {
         var r = romanizer.ator(1500);
         expect(r).to.be.equal('MD');
     });
-
     it('should return MCC given 1200', function () {
         var r = romanizer.ator(1200);
         expect(r).to.be.equal('MCC');
@@ -54,16 +38,38 @@ describe('romanizer', () => {
         var r = romanizer.ator(600);
         expect(r).to.be.equal('DC');
     });
-    // it('should return III given 3', function() {
-    //     var r = romanizer.ator(3);
-    //     expect(r).to.be.equal('III');
-    // });
-    // it('should return IV given 4', function() {
-    //     var r = romanizer.ator(4);
-    //     expect(r).to.be.equal('IV');
-    // });
-    // if('should return VI given 6', function() {
-    //     var r = romanizer.ator(6);
-    //     expect(r).to.be.equal('VI');
-    // });
+    // tens
+    it('should return X given 10', function () {
+        var r = romanizer.ator(10);
+        expect(r).to.be.equal('X');
+    });
+    it('should return XX given 20', function () {
+        var r = romanizer.ator(10);
+        expect(r).to.be.equal('X');
+    });
+    it('should return L given 50', function () {
+        var r = romanizer.ator(50);
+        expect(r).to.be.equal('L');
+    });
+    it('should return MCX given 1110', function () {
+        var r = romanizer.ator(10);
+        expect(r).to.be.equal('X');
+    });
+    // ones
+    it('should return I given 1', function () {
+        var r = romanizer.ator(1);
+        expect(r).to.be.equal('I');
+    });
+    it('should return V given 5', function () {
+        var r = romanizer.ator(5);
+        expect(r).to.be.equal('V');
+    });
+    it('should return III given 3', function() {
+        var r = romanizer.ator(3);
+        expect(r).to.be.equal('III');
+    });
+    if('should return VI given 6', function() {
+        var r = romanizer.ator(6);
+        expect(r).to.be.equal('VI');
+    });
 })
